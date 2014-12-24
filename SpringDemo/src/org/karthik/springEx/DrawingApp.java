224e7e3982f4f6b28f1db69f802e0f3e6a243a1c
Karthik <karthikeyan@global.com>
2014-12-24 19:14:22 +0530

    Initial Commit

---
+++ SpringDemo/src/org/karthik/springEx/DrawingApp.java
@@ -0,0 +1,25 @@
+package org.karthik.springEx;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Triangle triangle = new Triangle();
+		// BeanFactory factory = new XmlBeanFactory(new
+		// FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
+		triangle.draw();
+
+	}
+
+}
